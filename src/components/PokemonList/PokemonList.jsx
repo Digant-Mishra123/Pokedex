8793f8c91ceb83952e66bbfa0b58ee55f132ef2c
Digant-Mishra123 <digantmishra16@gmail.com>
2024-07-20 20:34:21 +0530

    Added CSS to pokedex

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -14,13 +14,29 @@ function PokemonList() {
     const [pokemonList,setpokemonList] = useState([]);
     const [isLoading,setIsLoading] = useState(true)
 
+    const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon"
+
+
     async function downloadPokemons() {
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon")
-        const pokemonResults = response.data.results;
-        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
-        const pokemonData = await axios.all(pokemonResultPromise)
-        console.log(pokemonData)
-        setpokemonList(pokemonData.map())
+        const response = await axios.get(POKEDEX_URL)//this downloads list of 20 pokemons
+        const pokemonResults = response.data.results;//We get the array of pokemons from result
+        console.log(pokemonResults)
+        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url)) //Iterating over the array of pokemons and using their URL to create an array of promises that will download those 20 pokemons
+        const pokemonData = await axios.all(pokemonResultPromise) // Passing that promise array to axios.all
+        console.log(pokemonData) //Array of 20 pokemon detailed data
+
+        //Iterate on the data of each pokemon and extracxt id,name,image and type
+        const pokeListResult = pokemonData.map((pokeData) => {
+            const pokemon = pokeData.data
+            return {
+                id:pokemon.id,
+                name:pokemon.name,
+                image:(pokemon.sprites.front_default) ? pokemon.sprites.front_default : pokemon.sprites.front_shiny,
+                types:pokemon.types
+            }
+        });
+        console.log(pokeListResult)
+        setpokemonList(pokeListResult)
         setIsLoading(false);
     }
 
@@ -38,10 +54,16 @@ function PokemonList() {
             </div> */}
 
             <div className="pokemon-list-wrapper">
-                <div>Pokemon List</div>
-                {(isLoading) ? 'Loading...':
-                        pokemonList.map((p) => <Pokemon name={p.name} image = {p.image} key={p.id}/>)
-                }
+
+                <div className="pokemon-wrapper">
+                    {isLoading ? 'Loading...':
+                        pokemonList.map((p) => <Pokemon name={p.name} image = {p.image} key={p.id} />)
+                    }
+                </div> 
+                <div className="controls">
+                    <button>Prev</button>
+                    <button>Next</button>
+                </div>
             </div>
         </>
     )
